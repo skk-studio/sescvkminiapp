8a9c4d472166b4899d30d2735af689629faec468
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-11-29 23:59:21 +0500

    Updates

---
+++ precache-manifest.e84f08b070d5f1b1d7ebdea0cbcd132d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5eb1599f623e75abe2e7f62efb3c5c",
+    "url": "/sescvkminiapp/index.html"
+  },
+  {
+    "revision": "9b62bf6281cb23033905",
+    "url": "/sescvkminiapp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "64bb61eda155cb0a3d6d",
+    "url": "/sescvkminiapp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9b62bf6281cb23033905",
+    "url": "/sescvkminiapp/static/js/2.127f8b0a.chunk.js"
+  },
+  {
+    "revision": "64bb61eda155cb0a3d6d",
+    "url": "/sescvkminiapp/static/js/main.2f85cff1.chunk.js"
+  },
+  {
+    "revision": "11af692d64d88fc10383",
+    "url": "/sescvkminiapp/static/js/runtime-main.8ec8a414.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/sescvkminiapp/static/media/persik.4e1ec840.png"
+  }
+]);
