064eada7f73e4f96a80b709621c9dd941e90c086
Tlmonko <ktkozlov>
2019-11-30 09:52:16 +0500

    Some changes

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -7,7 +7,7 @@ import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderBut
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
-import persik from '../img/persik.png';
+import persik from '../img/Goshan.jpg';
 import './Persik.css';
 
 const osName = platform();
