0f850491710bac3e008ac927cf25fe2c92a0c4a9
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-12-01 19:49:32 +0500

    some changes

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,7 +10,7 @@ import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Теперь App - это класс</PanelHeader>
+		<PanelHeader>App все еще функция</PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Connect">
 			<Cell
